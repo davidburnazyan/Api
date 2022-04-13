1cd0961c1c1b4fc9c68fca08886648b9fc0fdf86
davidburnazyan <david.burnazyan.96@gmail.com>
2022-04-13 22:22:08 +0400

    install jwt and create test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 const express = require("express");
+const jwt = require("jsonwebtoken");
+
 import { Request, Response } from "express";
 
 const app = express();
+
+app.use(express.json());
 
 app.get("/api", (req: Request, res: Response) => {
   res.json({
@@ -12,6 +16,21 @@ app.get("/api", (req: Request, res: Response) => {
 app.post("/api/posts", (req: Request, res: Response) => {
   res.json({
     message: "did work the post Api",
+  });
+});
+
+jwt;
+
+app.post("/api/login", (req: Request, res: Response) => {
+  const user = {
+    username: req.body.name,
+    email: req.body.email,
+  };
+
+  jwt.sign(user, "secretKey", (err: any, token: string) => {
+    res.json({
+      token,
+    });
   });
 });
 
