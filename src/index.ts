56c82afdad9ba438ba44ebf52c94d35e1923dcfe
David Burnazyan <davidburnazyan@Davids-MacBook-Pro.local>
2022-05-09 22:06:41 +0400

    db connecting

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+// import  {MongoClient} from  'mongodb';
+import mongoose from 'mongoose';
 
 import authRoute from "./routes/auth";
 import crudRoute from "./routes/crud";
@@ -7,6 +9,34 @@ import crudRoute from "./routes/crud";
 import { verifyToken } from "./middleware/verifyToken";
 
 const app = express();
+
+const mongoDB = "mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.3.1";
+mongoose.connect(mongoDB); // , { useNewUrlParser: true, useUnifiedTopology: true}
+
+const db = mongoose.connection;
+
+var Schema = mongoose.Schema;
+
+var SomeModelSchema = new Schema({
+  name: String,
+});
+
+const UserModel = mongoose.model('UserModel', SomeModelSchema );
+
+// UserModel.create({ name: 'David' })
+// .then(res => {
+//     console.log(res, '-----res');
+    
+// })
+
+UserModel.find()
+.then(res => {
+    console.log(res, '-----res');
+    
+})
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 dotenv.config();
 app.use(express.json());
