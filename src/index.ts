45d7f27a4991fd3fbc3d6d09629b727e10c76dc3
davidburnazyan <david.burnazyan.96@gmail.com>
2023-01-23 02:23:40 -0800

    commented all

--- src/index.ts
+++ src/index.ts
@@ -9,21 +9,21 @@ import { verifyToken } from "./middleware/verifyToken";
 const cors = require("cors");
 const app = express();
 
-dotenv.config();
-app.use(express.json());
-app.use(cors());
+// dotenv.config();
+// app.use(express.json());
+// app.use(cors());
 
-const prefix = "/api";
+// const prefix = "/api";
 
-const mongoDB = process.env.DB_URL || "";
+// const mongoDB = process.env.DB_URL || "";
 
-mongoose.connect('mongodb+srv://davidburnazyan:jXKHcYjkczynI4Ft@cluster0.xh5nagh.mongodb.net/?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://davidburnazyan:jXKHcYjkczynI4Ft@cluster0.xh5nagh.mongodb.net/?retryWrites=true&w=majority');
 
-const db = mongoose.connection; // 2
+// const db = mongoose.connection; // 2
 
-app.use(prefix, authRoute);
-// app.use(prefix, verifyToken, crudRoute);
-app.use(prefix, crudRoute);
+// app.use(prefix, authRoute);
+// // app.use(prefix, verifyToken, crudRoute);
+// app.use(prefix, crudRoute);
 
-db.on("error", console.error.bind(console, "MongoDB connection error:")); // 3
+// db.on("error", console.error.bind(console, "MongoDB connection error:")); // 3
 app.listen(5000, () => console.log("Server started on port 5000"));
