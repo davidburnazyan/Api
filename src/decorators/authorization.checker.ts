c17c5e9604c2789b0ec5fd4fa57df260aa9177aa
davidburnazyan <david.burnazyan.96@gmail.com>
2023-05-31 18:59:29 +0400

    copy code from another project and change by this case

---
+++ src/decorators/authorization.checker.ts
@@ -0,0 +1,58 @@
+import { Action } from 'routing-controllers';
+import { Exception } from '../modules/exeption';
+import { UserRepository } from '../repositories/user.repository';
+import { ClubRepository } from '../repositories/club.repository';
+import { MemberRepository } from '../repositories/member.repository';
+import { getCustomRepository } from 'typeorm';
+import { ErrorMessages, HttpStatusMessages, UserRoles } from '../enums';
+import { tokenParser } from '../helpers/token.parser';
+
+export const authorizationChecker = async (action: Action, roles: string[]): Promise<boolean> => {
+    const result =  tokenParser(action.request);
+    if (!result.success) {
+        throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: result.message })
+    }
+
+    const userRepository = getCustomRepository(UserRepository);
+    const user = await userRepository.findOne(result.userId);
+    if (!user)  {
+        throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.USER_NOT_FOUND })
+    }
+    if (!user.isVerified) {
+        throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.USER_IS_NOT_VERIFIED })
+    }
+
+    if (!user.isActive)  {
+        throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.USER_IS_NOT_ACTIVE })
+    }
+
+    if (user.role === UserRoles.CLUB) {
+        const clubRepository = getCustomRepository(ClubRepository);
+        const club = await clubRepository.findOne({ where: { userId: user.id } });
+        if (!club)  {
+            throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.CLUB_NOT_FOUND })
+        }
+        action.request.club = club ;
+    }
+
+    if (user.role === UserRoles.MEMBER) {
+        const memberRepository = getCustomRepository(MemberRepository);
+        const member = await memberRepository.findOne({ where: { userId: user.id } });
+        if (!member)  {
+            throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.MEMBER_NOT_FOUND })
+        }
+        action.request.member = member ;
+    }
+
+    if (roles && roles.length) {
+        if(!result.success) {
+            throw new Exception(HttpStatusMessages.BAD_REQUEST, { message: ErrorMessages.INVALID_TOKEN })
+        }
+        if (!roles.includes(user.role)) {
+            throw new Exception(HttpStatusMessages.FORBIDDEN, { message: ErrorMessages.FORBIDDEN });
+        }
+    }
+
+    action.request.user = user ;
+    return  true;
+};
---
+++ src/server/server.ts
@@ -0,0 +1,103 @@
+import express, { Express } from "express";
+
+import cors from 'cors';
+import { Server } from 'http';
+import { Inject, Service } from 'typedi';
+import * as bodyParser from 'body-parser';
+import { Action } from 'routing-controllers';
+import { rateLimit } from 'express-rate-limit';
+import { useExpressServer } from 'routing-controllers';
+import { authorizationChecker } from '../decorators/authorization.checker';
+// import { Config } from '../config';
+
+// import { ErrorHandlerMiddleware } from '../middlewares/error.handler.middleware';
+// import { ResponseInterceptor } from '../interceptors/response';
+// import * as swaggerUi from 'swagger-ui-express';
+// import { swaggerSpec } from '../modules/swagger';
+// @ts-ignore
+
+/** CONTROLLERS **/
+import { AuthController } from '../controllers/auth.controller';
+import { WordController } from '../controllers/word.controller';
+import { MessageController } from '../controllers/message.controller';
+
+@Service()
+export class ExpressServer {
+    // public constructor(@Inject() private readonly config: Config) { }
+    private server?: Express;
+    public httpServer!: Server;
+
+    public async setup(): Promise<Express> {
+
+        const server = express();
+        // this.setupStandardMiddlewares(server);
+        await this.configureApiEndpoints(server);
+        // await this.setupSwaggerMiddlewares(server);
+
+        this.httpServer = this.listen(server, 5000); // this.config.port
+        this.server = server;
+
+        return this.server
+    }
+
+    /**
+     * @param server
+     * @param port
+     */
+    public listen(server: Express, port: number) {
+        console.info(`Starting server on port ${port}`);
+        return server.listen(port);
+    }
+
+    /**
+     * @param server
+     */
+    setupStandardMiddlewares(server: Express): void {
+        server.set('trust proxy', true);
+
+        const limiter = rateLimit({
+            windowMs: 15 * 60 * 1000, // 15 minutes
+            max: 1000 // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+        });
+
+        server.use(bodyParser.json());
+        server.use(cors({ origin: '*' }));
+        server.use(bodyParser.urlencoded({ extended: true }));
+        server.use('/api', limiter)
+    }
+
+    public kill(): void {
+        if (this.httpServer) this.httpServer.close();
+    }
+
+    /**
+     * @param server
+     */
+    async configureApiEndpoints(server: Express): Promise<void> {
+        useExpressServer(server, {
+            authorizationChecker: (action: Action, roles: string[]) => authorizationChecker(action, roles),
+            routePrefix: '/api',
+            controllers: [
+                AuthController,
+                WordController,
+                MessageController,
+            ],
+            defaultErrorHandler: false,
+            // middlewares: [ErrorHandlerMiddleware],
+            // interceptors: [ResponseInterceptor]
+
+        });
+    }
+
+    /**
+     * @param server
+     */
+    // async setupSwaggerMiddlewares(server: Express) {
+    //     server.use(
+    //         '/api/docs',
+    //         swaggerUi.serve,
+    //         swaggerUi.setup(swaggerSpec())
+    //     );
+    // }
+}
+
