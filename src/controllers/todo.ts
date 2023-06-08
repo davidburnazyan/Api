c95a8c2883855461213b2b5a5279dbbcda41cbbc
davidburnazyan <david.burnazyan.96@gmail.com>
2023-06-08 17:48:53 +0400

    fox some logic

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -12,7 +12,11 @@ export const get = async (req: Request, res: Response) => {
 }
 
 export const create = async (req: Request, res: Response) => {
-  const response = await ToDo.create({ payload: req.body.payload })
+  const id = req.body.id;
+  const title = req.body.title;
+  const image = req.body.image;
+
+  const response = await ToDo.create({ id, title, image })
 
   return res.json({
     message: 'Created',
--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,7 +1,13 @@
 import mongoose from "mongoose";
 
 const ToDo = new mongoose.Schema({
-  payload: {
+  id: {
+    type: String,
+  },
+  title: {
+    type: String,
+  },
+  image: {
     type: String,
   },
 });
